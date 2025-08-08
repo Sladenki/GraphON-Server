2563f718c39d75f1ef050318ce369021f6e46daf
MarkSakhar <mark.sahar18@mail.ru>
2025-08-08 22:14:40 +0200

    fix createGraph

--- src/graph/dto/create-graph.dto.ts
+++ src/graph/dto/create-graph.dto.ts
@@ -1,9 +1,14 @@
 import { Types } from "mongoose"
-import { IsString, IsOptional } from 'class-validator';
+import { IsString, IsOptional, IsMongoId } from 'class-validator';
 
 export class CreateGraphDto {
+   @IsString()
    name: string
+   
+   @IsMongoId()
    parentGraphId: Types.ObjectId
+   
+   @IsMongoId()
    globalGraphId: Types.ObjectId
    
    @IsString()
