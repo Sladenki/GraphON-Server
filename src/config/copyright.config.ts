c9fb116b7ad159ca5fd0ac1d2c63b33361b2e31a
MarkSakhar <mark.sahar18@mail.ru>
2025-11-19 22:17:39 +0200

    change agreement copyright pdf

--- src/config/copyright.config.ts
+++ src/config/copyright.config.ts
@@ -1,5 +1,5 @@
 import { ConfigService } from '@nestjs/config';
 
 export const getCopyrightConfig = (configService: ConfigService) => ({
-  pdfPath: configService.get('COPYRIGHT_AGREEMENT_PDF_PATH') || './documents/agreement.pdf',
+  pdfPath: configService.get('COPYRIGHT_AGREEMENT_PDF_PATH') || './documents/Согласие на обработку ПД.pdf',
 }); 
