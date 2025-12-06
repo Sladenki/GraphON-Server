5dbc319a52485d4dcbfa157eb5cf8e2f52e1dda8
MarkSakhar <mark.sahar18@mail.ru>
2025-12-07 00:20:15 +0200

    fix chalk

--- src/logging.middleware.ts
+++ src/logging.middleware.ts
@@ -1,7 +1,18 @@
 import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 
-const chalk = require('chalk');
+// Динамический импорт для поддержки ES модуля chalk 5.x
+let chalkInstance: any = null;
+const getChalk = async () => {
+    if (!chalkInstance) {
+        const chalkModule = await import('chalk');
+        chalkInstance = chalkModule.default || chalkModule;
+    }
+    return chalkInstance;
+};
+
+// Инициализируем chalk при загрузке модуля
+const chalkPromise = getChalk();
 
 // Каждый раз, когда поступает HTTP-запрос, middleware фиксирует метод запроса (например, GET, POST), URL и текущее время.
 // Chalk раскрашивает цвет вывода 
@@ -14,13 +25,14 @@ export class LogginMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         const { method, originalUrl: url } = req;
         const reqTime = new Date().getTime();
-        res.on('finish', () => {
+        res.on('finish', async () => {
             const { statusCode } = res;
             const resTime = new Date().getTime();
             const logMessage = `${method} ${url} ${statusCode} - ${resTime - reqTime} ms`;
 
             // Успешный запрос (зеленый)
             if (statusCode === 201 || statusCode === 200) {
+                const chalk = await chalkPromise;
                 this.logger.log(chalk.cyan(logMessage)); 
             } 
         });
