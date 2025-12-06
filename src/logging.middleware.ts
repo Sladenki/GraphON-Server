5277b8cfbf913b220f1bc7e5613bfe765a520e3f
MarkSakhar <mark.sahar18@mail.ru>
2025-12-07 00:27:06 +0200

    change chalk logic

--- src/logging.middleware.ts
+++ src/logging.middleware.ts
@@ -1,21 +1,15 @@
 import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 
-// Динамический импорт для поддержки ES модуля chalk 5.x
-let chalkInstance: any = null;
-const getChalk = async () => {
-    if (!chalkInstance) {
-        const chalkModule = await import('chalk');
-        chalkInstance = chalkModule.default || chalkModule;
-    }
-    return chalkInstance;
+// ANSI escape коды для цветов (альтернатива chalk, работает без зависимостей)
+const colors = {
+    cyan: (text: string) => `\x1b[36m${text}\x1b[0m`,
+    green: (text: string) => `\x1b[32m${text}\x1b[0m`,
+    red: (text: string) => `\x1b[31m${text}\x1b[0m`,
 };
 
-// Инициализируем chalk при загрузке модуля
-const chalkPromise = getChalk();
-
 // Каждый раз, когда поступает HTTP-запрос, middleware фиксирует метод запроса (например, GET, POST), URL и текущее время.
-// Chalk раскрашивает цвет вывода 
+// Цветное логирование через ANSI escape коды
 @Injectable()
 export class LogginMiddleware implements NestMiddleware {
     logger = new Logger('Response')
@@ -25,15 +19,14 @@ export class LogginMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         const { method, originalUrl: url } = req;
         const reqTime = new Date().getTime();
-        res.on('finish', async () => {
+        res.on('finish', () => {
             const { statusCode } = res;
             const resTime = new Date().getTime();
             const logMessage = `${method} ${url} ${statusCode} - ${resTime - reqTime} ms`;
 
-            // Успешный запрос (зеленый)
+            // Успешный запрос (cyan цвет)
             if (statusCode === 201 || statusCode === 200) {
-                const chalk = await chalkPromise;
-                this.logger.log(chalk.cyan(logMessage)); 
+                this.logger.log(colors.cyan(logMessage)); 
             } 
         });
         next();
